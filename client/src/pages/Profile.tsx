7759541d1514fafa81a2dcc2c8e9bacc03792dce
Joshua Barua <joshuabarua9@hotmail.com>
2024-02-23 19:05:04 +0100

    Add disable  unused vars

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 import React, {ChangeEvent, SetStateAction, useContext, useEffect, useState} from 'react';
 // import Modal from '@mui/material/Modal'; // Material-UI modal
 import Button from '@mui/material/Button'; // Material-UI button
